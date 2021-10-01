aaebe412417ff9e77225f55dc73387c1b411f40f
SpaceDadNewman <91348837+SpaceDadNewman@users.noreply.github.com>
2021-10-01 17:55:05 -0600

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,17 @@ namespace cse210.td03
     {
         static void Main(string[] args)
         {
+            string chase = "";
             Console.WriteLine("Welcome to our collaborative program - Benjamin Bell");
+
+            displayGreetingWilcox();
+
+            string displayGreetingWilcox()
+            {
+                chase = ("Hello from Chase Wilcox");
+                Console.WriteLine(chase);
+                return chase;
+            }
         }
     }
 }
